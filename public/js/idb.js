b88491253c170388d9091904f8a986d38740b4f4
araceli <carmona.araceli.4690@gmail.com>
2021-08-26 17:30:25 -0700

    added service worker & web manifest

---
+++ public/js/idb.js
@@ -0,0 +1,72 @@
+//varaible to holf db connection
+let db;
+//establish connection
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+    //save a refrence to the database
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    //when db is successfully created with object store, save reference to db in global variable
+    db = event.target.result;
+    //check if app is online, if yes run function
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+//function will be exxecuted if no internet connection is available
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    //add record to store
+    budgetObjectStore.onupgradeneeded(record);
+}
+
+function uploadBudget() {
+    //open transaction on db
+    const transaction = db.transaction(['new_budget', 'readwrite']);
+    //access object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    //get all records from store
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+                    //access new_budget object store
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+                    //clear all item in store
+                    budgetObjectStore.clear();
+                    alert('All saved transactions have been submitted');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+//listen for app coming back online
+window.addEventListener('online', uploadBudget);
---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+//files to cache
+const FILES_TO_CACHE = [
+    "./public/index.html",
+    "./public/css/styles.css",
+    "./public/js/index.js"
+]
+
+//install service worker
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+//telling service worker how to manage caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+})
+
+//telling app how to retrieve info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request: ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache: ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not caches, fetching: ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
